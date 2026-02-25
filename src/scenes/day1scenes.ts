7427030e9854348fe9210d4b87eb532079ed2d0d
user <0901jp@example.com>
2026-02-25 10:04:29 +0900

    Final path fix2 for deployment

--- src/scenes/day1scenes.ts
+++ src/scenes/day1scenes.ts
@@ -26,7 +26,7 @@ export class Day1Scene extends Phaser.Scene{
     }
 
     preload(){
-        this.load.image("bg_train", "src/assets/電車内４.png")
+        this.load.image("bg_train", "src/assets/train.png")
     }
 
     create(){
